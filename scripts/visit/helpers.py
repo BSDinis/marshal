47e8c201aee184869e2505f283cd1d788b228754
BSDinis <baltasardinis@hotmail.com>
2019-06-21 14:16:59 +0100

    mid way through

--- scripts/visit/helpers.py
+++ scripts/visit/helpers.py
@@ -2,17 +2,6 @@
 
 from syntax.ast import add_type
 
-def real_types(ast):
-    def find_base(mappings, t):
-        while t in mappings:
-            t = mappings[t];
-        return t
-
-    typedefs = {m['new']: m['old'] for m in ast['typedefs']}
-    mappings = {typ: find_base(typedefs, typ) for typ in ast['types']}
-    return mappings
-
-
 def linearize_type(t):
     return t.replace(' ', '_').replace('[', '_').replace(']', '')
 
---
+++ test/test20.m
@@ -0,0 +1,2 @@
+typedef int fake_type[55];
+fake_type;
